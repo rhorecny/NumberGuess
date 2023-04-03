4b1bc6d50b89517df5f8724fa52f9662c935af73
rhorecny <129782304+rhorecny@users.noreply.github.com>
2023-04-03 13:26:55 -0400

    Update script.js
    Removed notes

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = 'Correct Number!';
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
@@ -20,7 +10,7 @@ const displayMessage = function (message) {
 };
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+
   //no input
   if (!guess) {
     displayMessage('No Number!');
